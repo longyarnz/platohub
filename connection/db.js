7e77ab80c8b32d9c36cecfd86ca9647ae0bf3a1a
Olalekan Ayodele <longyarnz@gmail.com>
2018-11-09 14:56:05 +0100

    Edit pipeline locally

--- connection/db.js
+++ connection/db.js
@@ -12,8 +12,6 @@ config();
  * @description Creates a connection to the MongoDB server
  */
 mongoose.Promise = promise;
-// mongodb://127.0.0.1:27017/bucketdb
-// mongodb+srv://lekan:forntenddeveloper@cluster0-dydyx.mongodb.net/bucket?retryWrites=true
 mongoose.connect(process.env.MONGODB, { useNewUrlParser: true });
 
 export default mongoose;
